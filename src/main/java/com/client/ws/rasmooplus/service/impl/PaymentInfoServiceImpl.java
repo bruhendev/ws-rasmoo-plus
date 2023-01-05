dfc6c9ef8e29da8e00127b157a87206b98ed0420
Bruno Henrique <brunohenrique242@gmail.com>
2023-01-05 13:15:22 -0300

    salvando as informaçoes de pagamento

--- src/main/java/com/client/ws/rasmooplus/service/impl/PaymentInfoServiceImpl.java
+++ src/main/java/com/client/ws/rasmooplus/service/impl/PaymentInfoServiceImpl.java
@@ -3,12 +3,20 @@ package com.client.ws.rasmooplus.service.impl;
 import java.util.Objects;
 import java.util.Optional;
 
+import com.client.ws.rasmooplus.dto.wsraspay.OrderDto;
+import com.client.ws.rasmooplus.dto.wsraspay.PaymentDto;
+import com.client.ws.rasmooplus.mapper.wsraspay.CreditCardMapper;
+import com.client.ws.rasmooplus.mapper.wsraspay.OrderMapper;
+import com.client.ws.rasmooplus.mapper.wsraspay.PaymentMapper;
 import org.springframework.stereotype.Service;
 
 import com.client.ws.rasmooplus.dto.PaymentProcessDto;
+import com.client.ws.rasmooplus.dto.wsraspay.CustomerDto;
 import com.client.ws.rasmooplus.exception.BusinessException;
 import com.client.ws.rasmooplus.exception.NotFoundException;
+import com.client.ws.rasmooplus.integration.WsRaspayIntegration;
 import com.client.ws.rasmooplus.mapper.UserPaymentInfoMapper;
+import com.client.ws.rasmooplus.mapper.wsraspay.CustomerMapper;
 import com.client.ws.rasmooplus.model.User;
 import com.client.ws.rasmooplus.model.UserPaymentInfo;
 import com.client.ws.rasmooplus.repositoy.UserPaymentInfoRepository;
@@ -20,28 +28,45 @@ public class PaymentInfoServiceImpl implements PaymentInfoService {
 
     private final UserRepository userRepository;
     private final UserPaymentInfoRepository userPaymentInfoRepository;
+    private final WsRaspayIntegration wsRaspayIntegration;
 
-    PaymentInfoServiceImpl(UserRepository userRepository, UserPaymentInfoRepository userPaymentInfoRepository) {
+    PaymentInfoServiceImpl(UserRepository userRepository, UserPaymentInfoRepository userPaymentInfoRepository,
+                           WsRaspayIntegration wsRaspayIntegration) {
         this.userRepository = userRepository;
         this.userPaymentInfoRepository = userPaymentInfoRepository;
+        this.wsRaspayIntegration = wsRaspayIntegration;
     }
 
     @Override
     public Boolean process(PaymentProcessDto dto) {
-        //verifica usuario por id e verifica se já existe assinatura
+        // verifica usuario por id e verifica se já existe assinatura
         Optional<User> userOpt = userRepository.findById(dto.getUserPaymentInfoDto().getId());
-        if(userOpt.isEmpty()){
+        if (userOpt.isEmpty()) {
             throw new NotFoundException("Usuário não encontrado");
         }
         User user = userOpt.get();
-        if(Objects.nonNull(user.getSubscriptionType())) {
+        if (Objects.nonNull(user.getSubscriptionType())) {
             throw new BusinessException("Pagamento não pode ser processado pois usuário já possui assinatura");
         }
 
-        //salvar informações de pagamento
-        UserPaymentInfo userPaymentInfo = UserPaymentInfoMapper.fromDtotoEntity(dto.getUserPaymentInfoDto(), user);
-        userPaymentInfoRepository.save(userPaymentInfo);
+        // cria ou atualiza usuario raspay
+        CustomerDto customerDto = wsRaspayIntegration.createCustomer(CustomerMapper.build(user));
 
+        //cria o pedido de pagamento
+        OrderDto orderDto = wsRaspayIntegration.createOrder(OrderMapper.build(customerDto.getId(), dto));
+
+        //processa o pagamento
+        PaymentDto paymentDto = PaymentMapper.build(customerDto.getId(), orderDto.getId(), CreditCardMapper.build(dto.getUserPaymentInfoDto(), user.getCpf()));
+
+        Boolean successPayment = wsRaspayIntegration.processPayment(paymentDto);
+
+        if (successPayment) {
+            //salvar informações de pagamento
+            UserPaymentInfo userPaymentInfo = UserPaymentInfoMapper.fromDtotoEntity(dto.getUserPaymentInfoDto(), user);
+            userPaymentInfoRepository.save(userPaymentInfo);
+        }
+        //enviar email de criacao de conta
+        //retornar o sucesso ou nao do pagamento
         return null;
     }
 
